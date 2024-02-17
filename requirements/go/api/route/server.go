eb3c1a244744e3cabb972fa741a1d78c59e1b61f
Davy Paulino <37334813+ThreeDP@users.noreply.github.com>
2024-02-17 15:36:09 +0000

    feat | test: WIP Create function for handler path and test then

--- requirements/go/api/route/server.go
+++ requirements/go/api/route/server.go
@@ -1,10 +1,11 @@
 package route
 
 import (
-	"net/http"
-	"strconv"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
 type BankServer struct {
@@ -31,20 +32,67 @@ func (db *DBQueries) GetTransations(id int) ([]byte, error) {
 }
 
 func (s *BankServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	lc := len("/clientes/")
-	le := len("/extrato")
-	lp := len(r.URL.Path)
-	id, err := strconv.Atoi(r.URL.Path[lc : lp - le])
-	if err != nil {
-		return
+	switch r.Method {
+		case http.MethodPost:
+			s.postTransation(w, r)
+		case http.MethodGet:
+			s.getTransations(w, r)
 	}
-	res, err := s.Store.GetTransations(id)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
+}
+
+func (s *BankServer) postTransation(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (s *BankServer) splitFields(str string, step string) []string {
+	parts := strings.Split(str, step)
+	var res []string
+	for _, part := range parts {
+		if len(part) > 0 {
+			res = append(res, part)
+		} 
+	}
+	return res
+}
+
+func (s *BankServer) parseURLPath(uri, path string) (map[string]string, bool) {
+	uriKeys := map[string]string{}
+	sUri, sPath := s.splitFields(uri, "/"), s.splitFields(path, "/")
+	if len(sUri) == len(sPath) {
+		for i, u := range sUri {
+			key, ok := strings.CutPrefix(u, ":")
+			field := u
+			if ok {
+				_, ok := uriKeys[key]
+				if !ok {
+					uriKeys[key] = sPath[i]
+				}
+				field = uriKeys[key]
+			}
+			if field != sPath[i] {
+				return nil, false
+			}
+		}
+		return uriKeys, true
+	}
+	return nil, false
+}
+
+func (s *BankServer) getTransations(w http.ResponseWriter, r *http.Request) {
+	uri, thisPath := s.parseURLPath("/clientes/:id/extrato", r.URL.Path)
+	if thisPath {
+		id, err := strconv.Atoi(uri["id"])
+		if err != nil {
+			return
+		}
+		res, err := s.Store.GetTransations(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(res)
+			return 
+		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(res)
-		return 
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
 }
--- requirements/go/api/route/server_test.go
+++ requirements/go/api/route/server_test.go
@@ -2,11 +2,12 @@ package route
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
-	"errors"
 )
 
 type TestBankStore struct {
@@ -66,6 +67,141 @@ func TestGetTransation(t *testing.T) {
 
 		checkStatusCode(t, response.Code, http.StatusNotFound)
 	})
+}
+
+func TestPostTransation(t *testing.T) {
+	store := TestBankStore{}
+	store.New()
+	server := &BankServer{&store}
+
+	t.Run("return statusOK when create a transation for user id 1", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/clientes/1/transacoes", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		checkStatusCode(t, response.Code, http.StatusOK)
+	})
+}
+
+func TestParseURLPath(t *testing.T) {
+	store := TestBankStore{}
+	store.New()
+	server := &BankServer{&store}
+
+	t.Run("Test pass a normal Path", func(t *testing.T) {
+		uri := "/client/info"
+		path := "/client/info"
+
+		received, thisPath := server.parseURLPath(uri, path)
+		expected := map[string]string{}
+
+		compareMapStrings(t, received, expected)
+		if thisPath == false {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("Test pass a normal Path with :id", func(t *testing.T) {
+		uri := "/client/:id/bill"
+		path := "/client/123/bill"
+
+		received, thisPath := server.parseURLPath(uri, path)
+		expected := map[string]string{"id": "123"}
+
+		compareMapStrings(t, received, expected)
+		if thisPath == false {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("Test pass two equal :id in same path return only first :id value", func(t *testing.T) {
+		uri := "/client/:id/:id"
+		path := "/client/123/123"
+
+		received, thisPath := server.parseURLPath(uri, path)
+		expected := map[string]string{"id": "123"}
+
+		compareMapStrings(t, received, expected)
+		if thisPath == false {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("Test pass two equal :id in different path return only first :id value", func(t *testing.T) {
+		uri := "/client/:id/:id"
+		path := "/client/123/321"
+
+		received, thisPath := server.parseURLPath(uri, path)
+
+		if received != nil {
+			t.Errorf("Expected nil, but has %v", received)
+		}
+		if thisPath == true {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("Test pass :id and :idBill returns map with two values", func(t *testing.T) {
+		uri := "/client/:id/:idBill"
+		path := "/client/123/321"
+
+		received, thisPath := server.parseURLPath(uri, path)
+		expected := map[string]string{"id": "123", "idBill": "321"}
+
+		compareMapStrings(t, received, expected)
+		if thisPath == false {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("validade path amount directorys", func(t *testing.T){
+		uri := "/client/:id/:idBill"
+		path := "/client/123/321/nopath"
+
+		received, thisPath := server.parseURLPath(uri, path)
+
+		if received != nil {
+			t.Errorf("Expected nil, but has %v", received)
+		}
+		if thisPath == true {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("validade path amount directorys with extra /", func(t *testing.T){
+		uri := "/client/:id/:idBill"
+		path := "/client/123/321/"
+
+		received, thisPath := server.parseURLPath(uri, path)
+		expected := map[string]string{"id": "123", "idBill": "321"}
+
+		compareMapStrings(t, received, expected)
+		if thisPath == false {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+
+	t.Run("validade path directory values", func(t *testing.T){
+		uri := "/client/"
+		path := "/login/"
+
+		received, thisPath := server.parseURLPath(uri, path)
+
+		if received != nil {
+			t.Errorf("Expected nil, but has %v", received)
+		}
+		if thisPath == true {
+			t.Error("the path is not the same as the uri")
+		}
+	})
+}
+
+func compareMapStrings(t *testing.T, received, expected map[string]string) {
+	t.Helper()
+	if reflect.DeepEqual(received, expected) == false {
+		t.Errorf("Expected %v, but has %v", expected, received)
+	}
 }
 
 func newRequestTransations(id int) *http.Request {
